682bb7af747264a60024411440f08723a246690a
Reza <81sedaghati@gmail.com>
2024-12-18 13:08:25 +0330

    Adding Bootstrap in Project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <h1>RezaSedaghati</h1>
-
+  <div class="alert alert-primary" role="alert">
+    A simple primary alert—check it out!
+  </div>
+  <div class="alert alert-secondary" role="alert">
+    A simple secondary alert—check it out!
+  </div>
   <router-view></router-view>
 </template>
 
