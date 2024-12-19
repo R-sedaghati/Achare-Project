7b8d9d26f69decfc2131dd5c2c89d0dec9b0b83d
Reza <81sedaghati@gmail.com>
2024-12-19 19:47:32 +0330

    Create Notif Section For Success Response

--- src/components/Address/AddressRegister.vue
+++ src/components/Address/AddressRegister.vue
@@ -1,150 +1,177 @@
 <template>
+  <template v-if="!showNotif">
+    <div
+      dir="rtl"
+      class="container-fluid bg-body-secondary"
+      style="height: 730px"
+    >
+      <template v-if="!showMap">
+        <p class="fs-2 ps-4 pt-3 pt-md-5 text-center">ثبت‌آدرس</p>
+        <div class="container bg-white hy-auto mt-4 mt-md-5">
+          <p class="fw-semibold pt-3 pt-md-5 text-center">
+            لطفا مشخصات و آدرس خود را وارد کنید
+          </p>
+          <form
+            @submit.prevent="handleSubmit"
+            class="row g-4 g-lg-5 mx-md-5 mb-md-5 mt-md-3"
+          >
+            <div class="col-12 col-md-4">
+              <label for="first_name" class="form-label">نام</label>
+              <input
+                type="text"
+                v-model="form.first_name"
+                class="form-control custom-focus p-md-3"
+                id="first_name"
+                placeholder="مثال: رضا"
+              />
+              <span class="text-danger" v-if="errors.first_name">{{
+                errors.first_name
+              }}</span>
+            </div>
+            <div class="col-12 col-md-4">
+              <label for="last_name" class="form-label">نام خوانوادگی</label>
+              <input
+                type="text"
+                v-model="form.last_name"
+                class="form-control custom-focus p-md-3"
+                id="last_name"
+                placeholder="مثال: رضایی"
+              />
+              <span class="text-danger" v-if="errors.last_name">{{
+                errors.last_name
+              }}</span>
+            </div>
+            <div class="col-12 col-md-4">
+              <label for="coordinate_mobile" class="form-label"
+                >شماره تلفن همراه</label
+              >
+              <input
+                type="text"
+                v-model="form.coordinate_mobile"
+                class="form-control custom-focus p-md-3"
+                id="coordinate_mobile"
+                placeholder="مثال: 09121234567"
+              />
+              <span class="text-danger" v-if="errors.coordinate_mobile">{{
+                errors.coordinate_mobile
+              }}</span>
+            </div>
+            <div class="col-12 col-md-5">
+              <label for="coordinate_phone_number" class="form-label"
+                >شماره تلفن ثابت(اختیاری)</label
+              >
+              <input
+                type="text"
+                v-model="form.coordinate_phone_number"
+                class="form-control custom-focus p-md-3"
+                id="coordinate_phone_number"
+                placeholder="مثال: 0211234567"
+              />
+              <span class="text-danger" v-if="errors.coordinate_phone_number">{{
+                errors.coordinate_phone_number
+              }}</span>
+            </div>
+            <div class="col-12 col-md-7">
+              <label for="address" class="form-label">آدرس</label>
+              <textarea
+                type="text"
+                v-model="form.address"
+                class="form-control custom-focus p-md-3"
+                id="address"
+                rows="1"
+                placeholder=""
+              ></textarea>
+              <span class="text-danger" v-if="errors.address">{{
+                errors.address
+              }}</span>
+            </div>
+            <div class="col-12 d-flex mb-4">
+              <p class="me-5">جنسیت</p>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  v-model="form.gender"
+                  value="female"
+                  name="flexRadioDefault"
+                  id="female"
+                />
+                <label class="form-check-label me-5" for="female"> خانم </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  v-model="form.gender"
+                  value="male"
+                  name="flexRadioDefault"
+                  id="male"
+                  checked
+                />
+                <label class="form-check-label" for="male"> آقا </label>
+              </div>
+            </div>
+          </form>
+        </div>
+      </template>
+      <template v-if="showMap">
+        <p class="fs-2 ps-4 pt-3 pt-md-5 text-center">انتخاب آدرس</p>
+        <div class="container bg-white hy-auto mt-3 mt-md-2 p-0">
+          <div>
+            <h5 class="text-center py-4">لطفاً موقعیت خود را انتخاب کنید:</h5>
+            <div id="map" style="height: 450px; width: 100%"></div>
+          </div>
+        </div>
+      </template>
+    </div>
+    <div v-if="!showMap">
+      <div class="col-12 d-grid pt-4 w-50 mx-auto mx-md-auto">
+        <button
+          @click="handleSubmit"
+          class="btn text-white p-2"
+          style="background-color: #00bfa6"
+        >
+          ثبت و ادامه
+          <div
+            v-if="loading"
+            class="spinner-border spinner-border-sm ms-2"
+          ></div>
+        </button>
+      </div>
+    </div>
+    <div v-if="showMap">
+      <div class="col-12 d-grid pt-4 w-50 mx-auto mx-md-auto">
+        <button
+          @click="handleSubmitToAPI"
+          class="btn text-white p-2"
+          style="background-color: #00bfa6"
+        >
+          <span v-if="!loading">ثبت و ادامه</span>
+          <div
+            v-if="loading"
+            class="spinner-border spinner-border-sm ms-2"
+          ></div>
+        </button>
+      </div>
+    </div>
+  </template>
   <div
     dir="rtl"
-    class="container-fluid bg-body-secondary"
-    style="height: 730px"
+    v-if="showNotif"
+    class="container-fluid bg-body-secondary d-flex justify-content-center align-items-center h"
+    style="height: 80vh"
   >
-    <template v-if="!showMap">
-      <p class="fs-2 ps-4 pt-3 pt-md-5 text-center">ثبت‌آدرس</p>
-      <div class="container bg-white hy-auto mt-4 mt-md-5">
-        <p class="fw-semibold pt-3 pt-md-5 text-center">
-          لطفا مشخصات و آدرس خود را وارد کنید
-        </p>
-        <form
-          @submit.prevent="handleSubmit"
-          class="row g-4 g-lg-5 mx-md-5 mb-md-5 mt-md-3"
+    <div class="container mx-auto">
+      <p class="text-center fw-bold fs-5">اطلاعات شما با موفقیت ثبت شد</p>
+      <div class="d-grid gap-2">
+        <button
+          class="btn btn-white w-50 mx-auto p-md-3"
+          style="color: #00bfa6; border: 4px solid #00bfa6"
+          type="button"
         >
-          <div class="col-12 col-md-4">
-            <label for="first_name" class="form-label">نام</label>
-            <input
-              type="text"
-              v-model="form.first_name"
-              class="form-control custom-focus p-md-3"
-              id="first_name"
-              placeholder="مثال: رضا"
-            />
-            <span class="text-danger" v-if="errors.first_name">{{
-              errors.first_name
-            }}</span>
-          </div>
-          <div class="col-12 col-md-4">
-            <label for="last_name" class="form-label">نام خوانوادگی</label>
-            <input
-              type="text"
-              v-model="form.last_name"
-              class="form-control custom-focus p-md-3"
-              id="last_name"
-              placeholder="مثال: رضایی"
-            />
-            <span class="text-danger" v-if="errors.last_name">{{
-              errors.last_name
-            }}</span>
-          </div>
-          <div class="col-12 col-md-4">
-            <label for="coordinate_mobile" class="form-label"
-              >شماره تلفن همراه</label
-            >
-            <input
-              type="text"
-              v-model="form.coordinate_mobile"
-              class="form-control custom-focus p-md-3"
-              id="coordinate_mobile"
-              placeholder="مثال: 09121234567"
-            />
-            <span class="text-danger" v-if="errors.coordinate_mobile">{{
-              errors.coordinate_mobile
-            }}</span>
-          </div>
-          <div class="col-12 col-md-5">
-            <label for="coordinate_phone_number" class="form-label"
-              >شماره تلفن ثابت(اختیاری)</label
-            >
-            <input
-              type="text"
-              v-model="form.coordinate_phone_number"
-              class="form-control custom-focus p-md-3"
-              id="coordinate_phone_number"
-              placeholder="مثال: 0211234567"
-            />
-            <span class="text-danger" v-if="errors.coordinate_phone_number">{{
-              errors.coordinate_phone_number
-            }}</span>
-          </div>
-          <div class="col-12 col-md-7">
-            <label for="address" class="form-label">آدرس</label>
-            <textarea
-              type="text"
-              v-model="form.address"
-              class="form-control custom-focus p-md-3"
-              id="address"
-              rows="1"
-              placeholder=""
-            ></textarea>
-            <span class="text-danger" v-if="errors.address">{{
-              errors.address
-            }}</span>
-          </div>
-          <div class="col-12 d-flex mb-4">
-            <p class="me-5">جنسیت</p>
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="radio"
-                v-model="form.gender"
-                value="female"
-                name="flexRadioDefault"
-                id="female"
-              />
-              <label class="form-check-label me-5" for="female"> خانم </label>
-            </div>
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="radio"
-                v-model="form.gender"
-                value="male"
-                name="flexRadioDefault"
-                id="male"
-                checked
-              />
-              <label class="form-check-label" for="male"> آقا </label>
-            </div>
-          </div>
-        </form>
+          <span class="fw-bold fs-5">مشاهده اطلاعات</span>
+        </button>
       </div>
-    </template>
-    <template v-if="showMap">
-      <p class="fs-2 ps-4 pt-3 pt-md-5 text-center">انتخاب آدرس</p>
-      <div class="container bg-white hy-auto mt-3 mt-md-2 p-0">
-        <div>
-          <h5 class="text-center py-4">لطفاً موقعیت خود را انتخاب کنید:</h5>
-          <div id="map" style="height: 450px; width: 100%"></div>
-        </div>
-      </div>
-    </template>
-  </div>
-  <div v-if="!showMap">
-    <div class="col-12 d-grid pt-4 w-50 mx-auto mx-md-auto">
-      <button
-        @click="handleSubmit"
-        class="btn text-white p-2"
-        style="background-color: #00bfa6"
-      >
-        ثبت و ادامه
-        <div v-if="loading" class="spinner-border spinner-border-sm ms-2"></div>
-      </button>
-    </div>
-  </div>
-  <div v-if="showMap">
-    <div class="col-12 d-grid pt-4 w-50 mx-auto mx-md-auto">
-      <button
-        @click="handleSubmitToAPI"
-        class="btn text-white p-2"
-        style="background-color: #00bfa6"
-      >
-        ثبت و ادامه
-        <div v-if="loading" class="spinner-border spinner-border-sm ms-2"></div>
-      </button>
     </div>
   </div>
 </template>
@@ -166,6 +193,7 @@ const form = reactive({
 const errors = reactive({});
 const loading = ref(false);
 const showMap = ref(false);
+const showNotif = ref(false);
 const selectedLocation = ref("No location selected");
 let mapInstance = null;
 
@@ -287,7 +315,7 @@ const handleSubmitToAPI = async () => {
     }
 
     const responseData = await response.json();
-    alert("اطلاعات با موفقیت ارسال شد!");
+    showNotif.value = true;
     console.log("Response from API:", responseData);
   } catch (error) {
     alert(`خطا: ${error.message}`);
